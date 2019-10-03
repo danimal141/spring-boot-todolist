43bcfd3bd0150e5a9efbefb6ba8d3112e7f5131a
danimal141 <hideaki.ishii1204@gmail.com>
2019-10-03 17:48:56 +0900

    Change to return template

--- src/main/kotlin/com/example/todolist/TaskController.kt
+++ src/main/kotlin/com/example/todolist/TaskController.kt
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 /**
  * Created by hideaki on 2019/09/28.
  */
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
     @GetMapping("")
@@ -18,6 +17,6 @@ class TaskController {
             Task(1, "Test", false)
         )
         model.addAttribute("tasks", tasks)
-        return tasks.toString()
+        return "tasks/index"
     }
 }
